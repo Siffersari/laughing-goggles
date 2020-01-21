0985e431ac5ff3cd10257f41ce525f93d8e5b55b
Leewel Karani <leewelkarani@gmail.com>
2020-01-21 22:36:36 +0300

    Add predefined response check for bot response

--- script.py
+++ script.py
@@ -1,11 +1,24 @@
 bot_template = "BOT : {0}"
 user_template = "USER : {0}"
+name = "Greg"
+weather = "cloudy"
+
+
+responses = {
+    "what's your name?": "my name is {0}".format(name),
+    "what's today's weather?": "the weather is {0}".format(weather),
+    "default": "default message"
+}
 
 
 def respond(message):
 
-    bot_message = "I can hear you! You said: " + message
+    if message in responses:
 
+        bot_message = responses[message]
+    else:
+
+        bot_message = responses["default"]
     return bot_message
 
 
@@ -19,6 +32,9 @@ def send_message(message):
 
 
 send_message("hello")
+send_message("what's today's weather?")
+send_message("what's your name?")
+send_message("what's your favorite color?")
 
 
 print(respond("hello!"))
