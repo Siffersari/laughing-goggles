edb21e8e4ef9ade99aea5a437206211cc11b0f21
Leewel Karani <leewelkarani@gmail.com>
2020-01-22 23:07:41 +0300

    Remove commented code

--- phraseextract.py
+++ phraseextract.py
@@ -15,42 +15,38 @@ rules = {'I want (.*)': ['What would it mean if you got {0}',
                      'Really--if {0}']}
 
 
-# Define replace_pronouns()
 def replace_pronouns(message):
 
     message = message.lower()
     if 'me' in message:
-        # Replace 'me' with 'you'
+
         return re.sub('me', 'you', message)
     if 'my' in message:
-        # Replace 'my' with 'your'
+
         return re.sub('my', 'your', message)
     if 'your' in message:
-        # Replace 'your' with 'my'
+
         return re.sub('your', 'my', message)
     if 'you' in message:
-        # Replace 'you' with 'me'
+
         return re.sub('you', 'me', message)
 
     return message
 
 
-# Define match_rule()
 def match_rule(rules, message):
     response, phrase = "default", None
 
-    # Iterate over the rules dictionary
     for pattern, responses in rules.items():
-        # Create a match object
+
         match = re.search(pattern, message)
         if match is not None:
-            # Choose a random response
+
             response = random.choice(responses)
             if '{0}' in response:
                 phrase = replace_pronouns(match.group(1))
-    # Return the response and phrase
+
     return response.format(phrase)
 
 
-# Test match_rule
 print(match_rule(rules, "do you remember your last birthday"))
