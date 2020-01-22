0bba7630fd494b85de96b397eecba8acda202423
Leewel Karani <leewelkarani@gmail.com>
2020-01-22 22:01:50 +0300

    Add grammar swap bot example

--- grammarswap.py
+++ grammarswap.py
@@ -3,13 +3,24 @@ import re
 
 def swap_pronouns(phrase):
 
-    if 'I' in phrase:
-        phrase = re.sub('I', 'you', phrase)
+    phrase = re.sub('I', 'you', phrase)
+    phrase = re.sub('you', 'I', phrase)
 
-    if 'my' in phrase:
-        phrase = re.sub('my', 'your', phrase)
+    phrase = re.sub('my', 'your', phrase)
+    phrase = re.sub('your', 'my', phrase)
 
     return phrase
 
 
-print(swap_pronouns("I walk my dog"))
+pattern = 'do you remember (.*)'
+
+message = 'do you remember when you ate strawberries in the garden'
+
+phrase = re.search(pattern, message).group(1)
+
+phrase = swap_pronouns(phrase)
+
+response = "how could I forget {}".format(phrase)
+
+
+print(response)
